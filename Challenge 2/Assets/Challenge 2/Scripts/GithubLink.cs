b6207af9d462da37c041c72f7c5c609b7371be2d
jakubdomurat <B00153270@mytudublin.ie>
2023-10-16 10:04:39 +0100

    B00153270

---
+++ Challenge 2/Assets/Challenge 2/Scripts/GithubLink.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubLink : MonoBehaviour
+{
+    public void openLink()
+    {
+        Application.OpenURL("https://github.com/jakubdomurat/B00153270");
+    }
+
+
+}
